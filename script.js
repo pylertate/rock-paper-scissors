dc2d0e52bdd3ca02c4aa5ee99bee35b73518a76b
Tyler Pate <tylerpate96@gmail.com>
2021-03-23 17:58:14 -0400

    Modified to add five rounds

--- script.js
+++ script.js
@@ -12,40 +12,120 @@ function comupterPlay() {
 
 function round(playerChoice, computerChoice) {
   // rock options
-  if (playerChoice.toLowerCase() == "rock" && computerChoice == "Scissors")
-    return "you win";
-  else if (playerChoice.toLowerCase() == "rock" && computerChoice == "Paper")
-    return "you lose";
-  else if (playerChoice.toLowerCase() == "rock" && computerChoice == "Rock")
-    return "It's a tie!";
+  if (playerChoice.toLowerCase() == "rock" && computerChoice == "Scissors") {
+    alert("You win!");
+    let result = 1;
+    return result;
+  } else if (
+    playerChoice.toLowerCase() == "rock" &&
+    computerChoice == "Paper"
+  ) {
+    alert("You Lose!");
+    let result = -1;
+    return result;
+  } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "Rock") {
+    alert("It's a tie!");
+    let result = 0;
+    return result;
+  }
+
   // scissors options
   else if (
     playerChoice.toLowerCase() == ("scissors" || "scissor") &&
     computerChoice == "Rock"
-  )
-    return "You Lose!";
-  else if (
+  ) {
+    alert("You Lose!");
+    let result = -1;
+    return result;
+  } else if (
     playerChoice.toLowerCase() == ("scissors" || "scissor") &&
     computerChoice == "Paper"
-  )
-    return "You Win!";
-  else if (
+  ) {
+    alert("You win!");
+    let result = 1;
+    return result;
+  } else if (
     playerChoice.toLowerCase() == ("scissors" || "scissor") &&
     computerChoice == "Scissors"
-  )
-    return "It's a tie!";
+  ) {
+    alert("It's a tie!");
+    let result = 0;
+    return result;
+  }
   // paper options
   else if (
     playerChoice.toLowerCase() == "paper" &&
     computerChoice == "Scissors"
-  )
-    return "You Lose!";
-  else if (playerChoice.toLowerCase() == "paper" && computerChoice == "Rock")
-    return "You Win!";
-  else if (playerChoice.toLowerCase() == "paper" && computerChoice == "Paper")
-    return "It's a Tie!";
-  else return "error";
+  ) {
+    alert("You Lose!");
+    let result = -1;
+    return result;
+  } else if (
+    playerChoice.toLowerCase() == "paper" &&
+    computerChoice == "Rock"
+  ) {
+    alert("You win!");
+    let result = 1;
+    return result;
+  } else if (
+    playerChoice.toLowerCase() == "paper" &&
+    computerChoice == "Paper"
+  ) {
+    alert("It's a tie!");
+    let result = 0;
+    return result;
+  } else alert("input: rock, paper, or scissors");
 }
 
-let playerChoice = prompt("lets play rock paper scissors!");
-alert(round(playerChoice, computerChoice));
+function game() {
+  function round1() {
+    let playerChoice = prompt("Choose your weapon: ");
+    let score = round(playerChoice, computerChoice);
+    return score;
+  }
+  score = round1();
+  alert(score);
+
+  function round2() {
+    let playerChoice = prompt("Choose your weapon: ");
+    let score = round(playerChoice, computerChoice);
+    return score;
+  }
+  score = score + round2();
+  alert(score);
+
+  function round3() {
+    let playerChoice = prompt("Choose your weapon: ");
+    let score = round(playerChoice, computerChoice);
+    return score;
+  }
+  score = score + round3();
+
+  if (score === 3) {
+    alert("Congratulations, you probably cheated!");
+  } else if (score !== 3) alert("You suck! Try again...");
+  else {
+    function round4() {
+      let playerChoice = prompt(
+        "Choose your weapon: rock, paper, or scissors! "
+      );
+      let score = round(playerChoice, computerChoice);
+      return score;
+    }
+    score = score + round4();
+    alert(score);
+    function round5() {
+      let playerChoice = prompt("FINAL ROUND");
+      let score = round(playerChoice, computerChoice);
+      return score;
+    }
+    score = score + round5();
+    if (score > 2) alert("Gg, you got lucky.");
+    else if (score === 2) alert("It's a tie, but I was just warming up.");
+    else alert("YOU LOSE. Try again?");
+  }
+}
+newGame = confirm("Would you like to play Rock, Paper, Scissors?");
+
+if (newGame == true) game();
+else alert("Chicken!!!");
